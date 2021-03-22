19d309fed202f0329ad6ad3a1082ddd01af747d2
Bruce MONGTHE <58480180+KazeoYa@users.noreply.github.com>
2021-03-22 20:59:21 +0100

    css fix

--- PluggedIn/views/viewDetailOffer.php
+++ PluggedIn/views/viewDetailOffer.php
@@ -11,14 +11,16 @@
             <p>Localité</p>
             <p>Nombres d'étudiants CESI déjà acceptés en stage</p>
         </div>
+    <div class = "bouton_crud"> 
         <div class="detail_aside">
             <input type="button" value="Ajouter à la wish-list">
-            <input type="button" value="Postuler à l'offre">
+            <input type="button" value="Postuler à l'offre    ">
         </div>
         <div class="detail_modif">
             <input type="button" value="Modifier">
             <input type="button" value="Supprimer">
         </div>
+    </div>
 
     <div class="detail_stats">
         <h2>Informations sur l'offre</h2> <br>
