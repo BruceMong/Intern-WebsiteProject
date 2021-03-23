f145b5603c438579e1a16b16e44437dd3c08884e
EricaGrace <75397881+EricaGrace@users.noreply.github.com>
2021-03-23 11:37:54 +0100

    Update viewAPropos.php

--- PluggedIn/views/viewAPropos.php
+++ PluggedIn/views/viewAPropos.php
@@ -6,11 +6,18 @@
 <div class=bloc_page>
 
     <article>
-        <h1>Information</h1>
+        <h1>Informations utiles</h1>
         <div class="conception">
             <h2> Conception </h2>
             <p>Architecture du site web: MVC (Model, View, Controller)</p>
             <img src="content/images/mvc-schema.png" alt="Banniere Home" />
+     <p>Modèle-vue-contrôleur ou MVC est un motif d'architecture logicielle destiné aux interfaces graphiques lancé en 1978 et très populaire pour les applications web.
+</br>Le motif est composé de trois types de modules ayant trois responsabilités différentes : les modèles, les vues et les contrôleurs.
+<ul>
+				<li>Un modèle (Model) contient les données à afficher.</li>
+				<li>Une vue (View) contient la présentation de l'interface graphique.</li>
+				<li>Un contrôleur (Controller) contient la logique concernant les actions effectuées par l'utilisateur.</li>
+			</ul>
             <p> L’approche MVC apporte de réels avantages:
                 </br> Une conception claire et efficace grâce à la séparation des données de la vue et du contrôleur
                 </br>Un gain de temps de maintenance et d’évolution du site
@@ -21,7 +28,7 @@
 
 
         <div class="icons-intro">
-            <h2> Développeur: </h2>
+            <h2> Développeurs: </h2>
             <ul>
                 <li>
                     <img src="content/images/hadrien.jpg">
