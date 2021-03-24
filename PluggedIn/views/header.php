be6ab2a024d49525be58470be8108bb76d644a50
KazeoYa <58480180+KazeoYa@users.noreply.github.com>
2021-03-24 16:49:08 +0100

    fsdf

--- PluggedIn/views/header.php
+++ PluggedIn/views/header.php
@@ -8,14 +8,14 @@ session_start();
 
 <head>
     <meta charset="utf-8" />
+
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="content/css/style.css" />
     <link rel="stylesheet" href="content/css/nav.css" />
     <link rel="stylesheet" href="content/css/header.css" />
     <link rel="stylesheet" href="content/css/footer.css" />
-
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
 
     <title><?= $t ?></title>
 </head>
