ea782442ca48cbdadf0578b78d1fcc95000e8d41
Hadrien Pinoteau <hadrien.pinoteau@viacesi.fr>
2021-03-24 11:32:36 +0100

    C̛̣̥͍̤ͨ͐͊ͫ̑̿̄̅̀͞S̶̡̢̯͙̠̪̱̼̥̱̈S̵̡̭̞̼̱̘̋̀͠ ̧̛̞͓͎̮̙̋ͨ̌̀̚B̧̝̜̜̞̝̣͉̿͗ͪ̈́̆͐́́ͅE̯̳̮̓A̸̩̜̹̟̫̬ͬ̑̓̑̾̔U̝̾ͣͨ̄ͫ̌̚͘

--- PluggedIn/views/header.php
+++ PluggedIn/views/header.php
@@ -54,7 +54,7 @@ session_start();
         <div class="header_right" id="identification">
             <ul>
                 <li>
-                    <?php if (empty($_SESSION['utilisateur'])) {
+                    <?php if (!empty($_SESSION['utilisateur'])) {
                         echo '<a class="login" href="login">';
                         echo '<input type="button" value="Login">';
                         echo '</a>';
--- PluggedIn/views/viewDetailOffer.php
+++ PluggedIn/views/viewDetailOffer.php
@@ -6,7 +6,7 @@
 
 
 
-        <div class="detail_presentation">
+    <div class="detail_presentation">
             <img src="<?= $entreprise->image() ?>" alt="image entreprise" width="100px" height="100px">
             <h2>Nom de l'entreprise: </h2> <input type="text" class="info_modif" value="<?= $entreprise->nom() ?>">
             <p>Secteur d'activité : </p> <input type="text" class="info_modif" value="<?= $entreprise->secteur_activite() ?>">
@@ -28,23 +28,44 @@
                     <input type="button" value="Modifier">
                 </form>
                 <form action="" method="post">
-                    <input type="button" value="Supprimer">
+                    <input type="button" class="buttonsupp" href="#" onclick="show('popup')" value="Supprimer">
+                    <div class="popup" id="popup">
+                        <h3>Vous êtes sûr de modifier?</h3>
+                        <center>
+                        <a href="#" onclick="hide('popup')">Annuler</a>
+                        <a href="#" onclick="hide('popup')">Confirmer</a>
+                        </center>
+                    </div>
+
+                    <script>
+                        $ = function(id) {
+                            return document.getElementById(id);
+                        }
+
+                        var show = function(id) {
+                            $(id).style.display = 'block';
+                        }
+                        var hide = function(id) {
+                            $(id).style.display = 'none';
+                        }
+                    </script>
+
                 </form>
             </div>
         </div>
 
         <div class="detail_stats">
             <h2>Informations sur l'offre</h2> <br>
-            <p>Durée du stage : </p> <input type="text" class="info_modif" value="<?= $offer->duree_stage() ?>">
-            <p>Nombres de places offertes : </p> <input type="text" class="info_modif" value="<?= $offer->nombre_place() ?>">
-            <p>Base de rémunération : </p> <input type="text" class="info_modif" value="<?= $offer->base_remuneration() ?>">
-            <p>Date de l'offre : </p> <input type="text" class="info_modif" value="<?php $offer->date() ?>">
-            <p>Typesde promotions concernées : </p> <input class="info_modif" type="text" value="<?= $offer->type_promo_concerne() ?>">
+            <p>Durée du stage : <?= $offer->duree_stage() ?></p>
+            <p>Nombres de places offertes : <?= $offer->nombre_place() ?> </p>
+            <p>Base de rémunération : <?= $offer->base_remuneration() ?></p>
+            <p>Date de l'offre : <?php $offer->date() ?> </p>
+            <p>Typesde promotions concernées : <?= $offer->type_promo_concerne() ?></p>
 
         </div>
         <div class="detail_competence">
             <h2>Compétences requises : </h2>
-            <input type="text" class="info_modif" value="<?= $offer->competences() ?>">
+            <?= $offer->competences() ?>
         </div>
 
     </div>
