559b701545d3ce225d5079798c415eb4bf0dbaf6
KazeoYa <58480180+KazeoYa@users.noreply.github.com>
2021-03-24 12:27:25 +0100

    cwxc

---
+++ PluggedIn/controllers/controllerDetailEntreprise.php
@@ -0,0 +1,13 @@
+<?php
+// CRÉATION DE L'OBJET MANAGER
+// $modelArticle = new ModelArticle($bdd);
+
+// JE RÉCUPÈRE LA LISTE DE TOUS LES ARTICLES
+//$articles = $modelArticle->getArticles();
+
+$t = 'Détail de l\'entreprise';
+
+$modelEntreprise = new ModelEntreprise($bdd);
+$entreprise = $modelEntreprise->getEntreprise($_POST['id']);
+
+require_once('views/viewDetailOffer.php');
