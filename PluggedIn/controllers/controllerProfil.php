4d0aa77007d42042f307a157e4b2067436150f6c
KazeoYa <58480180+KazeoYa@users.noreply.github.com>
2021-03-25 16:20:47 +0100

    je répare ma merde

--- PluggedIn/controllers/controllerProfil.php
+++ PluggedIn/controllers/controllerProfil.php
@@ -12,6 +12,6 @@ $promotion = $modelPromotion->getPromotion($utilisateur->id_promotion());
 $modelDroit = new ModelDroit($bdd);
 $droit = $modelDroit->getDroit($utilisateur->id_users());
 
-$t = 'Profil '.$utilisateur->prenom();
+$t = 'Profil ' . $utilisateur->prenom();
 
 require_once('views/viewProfil.php');
