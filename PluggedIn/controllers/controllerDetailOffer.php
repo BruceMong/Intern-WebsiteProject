c730558a0c42800bfb19106393536358a610583d
Bruce MONGTHE <58480180+KazeoYa@users.noreply.github.com>
2021-03-26 18:52:54 +0100

    Update controllerDetailOffer.php

--- PluggedIn/controllers/controllerDetailOffer.php
+++ PluggedIn/controllers/controllerDetailOffer.php
@@ -11,19 +11,9 @@ if($_SESSION['droits'][0]->consulter_stats_offres() != 1)
     header('Location:' . URL . 'error');
 }
 
-$modelUtilisateur = new ModelUtilisateur($bdd);
-$utilisateurSession = $modelUtilisateur->getUtilisateur($_SESSION['utilisateur']);
-$modelDroit = new ModelDroit($bdd);
-$droitSession = $modelDroit->getDroit($utilisateur->id_users());
-
-if($droitSession->consulter_stats_offres() == 1)
-{
-
 $modelEntreprise = new ModelEntreprise($bdd);
 $modelOffer = new ModelOffer($bdd);
 $offer = $modelOffer->getOffer($_POST['id']);
 $entreprise = $modelEntreprise->getEntrepriseByName($offer->entreprise());
 
 require_once('views/viewDetailOffer.php');
-
-}
