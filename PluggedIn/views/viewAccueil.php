121c79aca324e6fc1485dca9ddff8864b37f02d0
Hadrien Pinoteau <hadrien.pinoteau@viacesi.fr>
2021-03-28 00:46:39 +0100

    hmar

--- PluggedIn/views/viewAccueil.php
+++ PluggedIn/views/viewAccueil.php
@@ -7,8 +7,31 @@
 
 
 <div class=bloc_page>
+    <div class="present_acceuil">
+        <h1>PluggedIn qu'est-ce que c'est ?</h1>
+        <br>
+        <p>Aujourd’hui, les étudiants effectuent leurs recherches de stage en entreprise en activant leurs réseaux
+            personnels et professionnels (linkedIn, anciennes promotions, etc.), en postulant à des offres ou en
+            envoyant des candidatures spontanées. </p>
+        <p>Afin de rendre cette étape de recherche de stage plus facile et pratique, PluggedIn est une plateforme qui regroupe différentes offres de stage, et qui permettra de stocker les données des
+            entreprises ayant déjà pris un stagiaire, ou qui en recherchent un.
+        </p>
+        <p>PluggedIn c'est tout simplement <strong> le moyen le plus simple de trouver un stage ...</strong>
+        </p>
+        <p>
+            PluggedIn permet d'informatiser l'aide à la recherche de stage regroupant toutes les offres de stage.
+            Il permet entre autre d'enregistrer des données des entreprises ayant déjà pris un stagiaire, ou qui en recherchent un.
+            Ce site permet donc de faciliter l'orientation des nouveaux étudiants dans leur recherche de stage
+        </p>
+        <p>
+            Les offres de stages sont notamment enregistrées par compétences, ce qui permet à l’étudiant de trouver un
+            stage en rapport avec son profil. L’application fournit différentes interfaces à destination des différents profils
+            d’utilisateur. Plusieurs catégories de fonctionnalités sont déployées sur la plateforme.
+        </p>
+        <center> <img src="https://arras.cesi.fr/wp-content/uploads/sites/9/2019/05/Cesi_Logo_INGENIEUR_RVB-HD-500x296.jpg" alt="">  <img src="https://edito.regionsjob.com/xjob/wp-content/uploads/sites/3/2018/10/cesi-ecole-dinge-2_opt.jpg" alt=""></center>
+        <?= var_dump($_SESSION['droits']); ?>
+    </div>
 
-    <?= var_dump( $_SESSION['droits'] ) ; ?>
 </div>
 
 
