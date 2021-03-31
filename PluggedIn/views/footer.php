6a80ad9c15587333763e3e6e2656897941774c66
Bruce MONGTHE <58480180+KazeoYa@users.noreply.github.com>
2021-03-31 08:54:25 +0200

    Update footer.php

--- PluggedIn/views/footer.php
+++ PluggedIn/views/footer.php
@@ -5,8 +5,6 @@
             <ul class="link_footer">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
-                <li><a href="#">Shop</a></li>
-                <li><a href="#">Totem animal</a></li>
             </ul>
         </div>
 
