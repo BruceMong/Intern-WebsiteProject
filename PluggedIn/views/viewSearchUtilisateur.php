168c28e1cf6649d3776b1c179b5cc2c03c395651
KazeoYa <58480180+KazeoYa@users.noreply.github.com>
2021-03-31 14:10:44 +0200

    Revert "Update viewSearchUtilisateur.php"
    This reverts commit 625bbececa3626ba1de8ce1edea5b695cfb0a4e1.

--- PluggedIn/views/viewSearchUtilisateur.php
+++ PluggedIn/views/viewSearchUtilisateur.php
@@ -16,49 +16,41 @@
 
     </header>
 
-    <div class="article-container">
+    <div class="article-container">   
         <?php foreach ($utilisateurs as $utilisateur) : ?>
             <form action="detailUtilisateur" method="post">
-                <button type="submit" class="button_to_detail" name="id" value="<?= $utilisateur->login() ?>">
+                <button type="submit" class="button_to_detail" name="id" value="<?= $utilisateur->login()?>">
                     <div class="article_box" style="height: 200;">
                         <p>Nom : <?= $utilisateur->nom() ?> </p>
                         <p>Prénom : <?= $utilisateur->prenom() ?></p>
                         <p>Login : <?= $utilisateur->login() ?> </p>
-                        <p>Promo : <?php $promoUtil = $modelPromotion->getPromotion($utilisateur->id_promotion());
-                                    echo $promoUtil->libelle();   ?> </p>
-                        <p>Centre : <?= $utilisateur->centre() ?> </p>
-                        <p>Role : <?php $profilUtil = $modelProfil->getProfil($utilisateur->id_profil());
-                                    echo $profilUtil->libelle()  ?> </p>
+                        <p>Promo : <?php $promoUtil = $modelPromotion->getPromotion($utilisateur->id_promotion()); echo $promoUtil->libelle();   ?>  </p>
+                        <p>Centre : <?= $utilisateur->centre() ?> </p> 
+                        <p>Role :   <?php $profilUtil = $modelProfil->getProfil($utilisateur->id_profil()); echo $profilUtil->libelle()  ?> </p>
                     </div>
                 </button>
             </form>
         <?php endforeach; ?>
     </div>
 
-<select name="entreprise">
     <nav class="nav_pagination">
         <ul class="pagination">
-            <form action="searchUtilisateur" method="post" class="form_pagination">
-                <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
-                <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
-                    <button type="submit" class="page-link" name="page" value="<?= $currentPage - 1 ?>"> Précédente </button>
+        <form action="searchUtilisateur" method="post" class = "form_pagination">
+            <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
+            <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                <button type="submit" class="page-link" name="page" value="<?= $currentPage - 1 ?>"> Précédente </button>
+            </li>
+            <?php for ($page = 1; $page <= $pages; $page++) : ?>
+                <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
+                <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                <button type="submit" class="page-link" name="page" value="<?= $page ?>"> <?= $page ?> </button>
                 </li>
-                <?php for ($page = 1; $page <= $pages; $page++) : ?>
-                    <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
-                    <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
-                        <button type="submit" class="page-link" name="page" value="<?= $page ?>"> <?= $page ?> </button>
-                    </li>
-                <?php endfor ?>
-                <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
-                <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
-                    <button type="submit" class="page-link" name="page" value="<?= $currentPage + 1 ?>"> Suivant </button>
-                </li>
+            <?php endfor ?>
+            <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
+            <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+                <button type="submit" class="page-link" name="page" value="<?= $currentPage + 1 ?>"> Suivant </button>
+            </li>
             </form>
-
-            <?php foreach ($entreprises as $entSelect) : ?>
-                <option value="<?= $entSelect->nom() ?>"><?= $entSelect->nom() ?></option>
-            <?php endforeach; ?>
-
         </ul>
     </nav>
 
